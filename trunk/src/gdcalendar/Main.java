05f1c6e1190ed2aacb5c58f391dd2e8f6f87a051
tomas.toss <tomas.toss@10d44bd6-9de5-023c-7bc7-d8ec78e54aae>
2010-03-27 23:40:21 +0000

    Finally got the repository working correctly (!!!)
    First commit for the project, initial layout for the calendar.
    Implemented a collapsible panel component, need some refinement

---
+++ trunk/src/gdcalendar/Main.java
@@ -0,0 +1,19 @@
+package gdcalendar;
+
+import gdcalendar.gui.MainWindow;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
+    }
+
+}
