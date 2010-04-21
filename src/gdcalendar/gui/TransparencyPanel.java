e182e5eefe17c8e5a830262c412466de41ab7999
jamhos <jamhos@10d44bd6-9de5-023c-7bc7-d8ec78e54aae>
2010-04-21 21:13:29 +0000

    Slider listeners completed, updates label value. Still no actual transparency though. :P

--- src/gdcalendar/gui/TransparencyPanel.java
+++ src/gdcalendar/gui/TransparencyPanel.java
@@ -5,15 +5,43 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.basic.BasicSliderUI.ChangeHandler;
 
 /**
  *
  * @author James
  */
 public class TransparencyPanel extends JPanel {
+    
+    private JSlider opacitySlider;
+    private JLabel percentLabel;
+
     public TransparencyPanel() {
         setLayout(new BorderLayout());
+        
+        opacitySlider = new JSlider(SwingConstants.VERTICAL, 0, 100, 100);
+        percentLabel = new JLabel(Integer.toString(opacitySlider.getValue()) + "%");
+
         add(new JLabel("Opacity"), BorderLayout.PAGE_START);
-        add(new JSlider(SwingConstants.VERTICAL, 0, 100, 100), BorderLayout.CENTER);
+        add(opacitySlider, BorderLayout.CENTER);
+        add(percentLabel, BorderLayout.PAGE_END);
+
+        initListeners();
+    }
+
+    private void initListeners() {
+
+        opacitySlider.addChangeListener(new ChangeListener() {
+
+            public void stateChanged(ChangeEvent e) {
+                TransparencyPanel.this.sliderChanged(e);
+            }
+        });
+    }
+
+    private void sliderChanged(ChangeEvent e) {
+        percentLabel.setText(Integer.toString(opacitySlider.getValue()) + "%");
     }
 }
