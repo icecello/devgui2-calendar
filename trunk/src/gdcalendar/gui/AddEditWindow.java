6cc75116ddecad0b1ab44b7bce9cd2381f1abdee
jamhos <jamhos@10d44bd6-9de5-023c-7bc7-d8ec78e54aae>
2010-05-18 15:09:51 +0000

    Added an AboutWindow, in the Help menu.

---
+++ trunk/src/gdcalendar/gui/AddEditWindow.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gdcalendar.gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author James
+ */
+public class AddEditWindow extends JFrame {
+
+}
